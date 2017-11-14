28ff581cd38e7fe97532105ec11b5eefad3f7c92
AmirAmine1992 <33393923+AmirAmine1992@users.noreply.github.com>
2017-11-14 22:19:48 +0100

    Codes de l'allumage des LED

---
+++ Parametre_Chenille_Speciale.ino
@@ -0,0 +1,31 @@
+int i=4, count = 4;
+bool boucle = false;
+void setup() {
+ 
+  while(i<=13){
+    pinMode(i, OUTPUT);
+    i++;  
+  }
+  i=4;
+}
+
+void loop() {
+ 
+  
+    for( i = 4; i<=13; i++ ){
+      digitalWrite(i, HIGH);
+      delay(10);
+      digitalWrite(i,LOW);
+    }
+
+   for( i = 13; i>=4; i-- ){
+      digitalWrite(i,HIGH);
+      delay(20);
+      digitalWrite(i,LOW);
+    }
+      digitalWrite(13,HIGH);
+      delay(20);
+      digitalWrite(13,LOW);
+    delay(500);
+     
+  }
